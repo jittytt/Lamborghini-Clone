70f8cc6f401ceb2b7b8d20651b3f0bf7cc0d41c3
Jitty Tresa Thomas <jittytresa@gmail.com>
2023-12-20 13:30:07 +0530

    fix: updated navbar, login page and js file

--- scripts/carousel-landingpage.js
+++ scripts/carousel-landingpage.js
@@ -2,8 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
   const slides = document.querySelectorAll('.carousel-slide');
   const totalSlides = slides.length;
   let currentSlide = 0;
-  let isDragging = false;
-  let startPositionX = 0;
 
   function showSlide(index) {
     slides.forEach((slide, i) => {
@@ -33,34 +31,6 @@ document.addEventListener('DOMContentLoaded', function () {
   // Event listeners for manual control
   document.getElementById('left-icon').addEventListener('click', prevSlide);
   document.getElementById('right-icon').addEventListener('click', nextSlide);
-
-  // Event listeners for dragging
-  slides.forEach((slide) => {
-    slide.addEventListener('mousedown', (e) => {
-      isDragging = true;
-      startPositionX = e.clientX;
-    });
-
-    slide.addEventListener('mousemove', (e) => {
-      if (isDragging) {
-        const deltaX = e.clientX - startPositionX;
-        if (deltaX > 50) {
-          nextSlide();
-          isDragging = false;
-        } else if (deltaX < -50) {
-          prevSlide();
-          isDragging = false;
-        }
-      }
-    });
-
-    slide.addEventListener('mouseup', () => {
-      isDragging = false;
-    });
-
-    // Change cursor to pointer when over the image
-    slide.style.cursor = 'pointer';
-  });
 
   // Function to apply rolling-in animation and blur effect to caption lines
   function applyAnimationAndBlur(slide) {
