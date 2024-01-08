ff4860abf95393dcece10d5680ca6911a826b7bb
ELENAMARIAVARGHESE <elenamariavarghese@gmail.com>
2024-01-09 02:16:12 +0530

    feat: Added typescript for the dashboard

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -7,19 +7,4 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
-function showSecondCheckbox() {
-  const generalSubscription = document.getElementById('generalSubscription');
-  const interestDiv = document.getElementById('interestDiv');
-  if (generalSubscription.checked) {
-      interestDiv.style.display = 'block';
-  } else {
-      interestDiv.style.display = 'none';
-      document.getElementById('interestedParty').checked = false; // Uncheck second checkbox if first is unchecked
-  }
-} 
 
-function checkSaveButton() {
-  console.log("Clicked");
-  const interestedParty = document.getElementById('saveButton');
-  interestedParty.classList.add("enable-save");
-}
